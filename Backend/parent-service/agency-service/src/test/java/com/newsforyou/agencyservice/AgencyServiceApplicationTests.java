96cc0e38ac5dcfce49838fa1ba76af576cbb073c
soumendas2252 <70625572+soumendas2252@users.noreply.github.com>
2023-08-23 18:33:23 +0530

    microservice implementation and news-service update

---
+++ Backend/parent-service/agency-service/src/test/java/com/newsforyou/agencyservice/AgencyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsforyou.agencyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgencyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
