a290cea1f66c02a89d00db257f5c602535eb8893
soumendas2252 <70625572+soumendas2252@users.noreply.github.com>
2023-08-30 21:58:49 +0530

    Cors issue fixed

--- Backend/parent-service/api-gateway/src/main/java/com/newsforyou/apigateway/ApiGatewayApplication.java
+++ Backend/parent-service/api-gateway/src/main/java/com/newsforyou/apigateway/ApiGatewayApplication.java
@@ -1,14 +1,11 @@
 package com.newsforyou.apigateway;
 
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcCongiurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -18,16 +15,14 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 	
-	@Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration apiCorsConfiguration = new CorsConfiguration();
-        apiCorsConfiguration.setAllowCredentials(true);
-        apiCorsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
-        apiCorsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
-        apiCorsConfiguration.setAllowedMethods(Collections.singletonList("*"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", apiCorsConfiguration);
-        return source;
-    }
+//	@Bean
+//	public WebMvcConfigurer corsConfig() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMapping(org.springframework.web.reactive.config.CorsRegistry registry) {
+//				registry.addMapping("/**").allowedOrigins("*");
+//			}
+//		};
+//	}
 }
+
